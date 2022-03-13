cc3c1a488c146d8bb44b1572423fd34fe338acb2
francis <franciskentris@yahoo.com>
2022-03-12 18:46:08 -0700

    updated

--- new.py
+++ new.py
@@ -1,60 +1,84 @@
-class Car:
-    def __init__(self, year, color, model):
-        self.year = year
-        self.color = color
-        self. model = model
-        return {self.year}, {self.color}, {self.model}
+empty_spots = {
+    1: 'open',
+    2: 'open',
+    3: 'open',
+    4: 'open',
+    5: 'open',
+    6: 'open',
+    7: 'open',
+    8: 'open',
+    9: 'open',
+    10: 'open'
+}
 
-class Garage:
-    def __init__(self):
-        self.cars_added = []
-        self.car_info = {}
-        self.spots = 10
-        self.bill = 0
+garage = {}
+
+class Parking():
     
     def spots_open(self):
-        return self.spots
+      print('This is the open parking:')
+      if empty_spots:
+          print(len(empty_spots))
+      else:
+          print('Sorry! No parking spots open')
     
-    def add_car(self, year, color, car):
-        self.car = car
-        self.tickets = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
-        if self.spots >= 0:
-            self.cars_added.append(str(car).split(' '))
-            self.spots -= 1
-            self.car_info = {'list':[], 'year': [], 'color': [], 'model':[]}
-            for index, m in enumerate(self.cars_added):
-                self.car_info['year'].append(self.tickets[index])
-                self.car_info['color'].append(m[1])
-                self.car_info['model'].append(m[2])
-            return "Thank you, car successfully added to parking"
-        else:
-            print(f"We have {self.spots} spots open.")
-            
-    def remove_car(self):
-        def __init__(self, my_list, bill_hours):
-            self.bill_hours = bill_hours
-            my_list = len(self.car_info['list'])
-            if my_list not in self.car_info['list']:
-                print("Sorry we could not find your car. Wanna check again?")
-                pass
-            for index, i in enumerate(self.car_info['list']):
-                            if i == my_list:
-                                print("Your car year is:", self.car_info['year'][index])
-                                print("Your car color is:", self.car_info['color'][index])
-                                print("Your car model is:", self.car_info['model'][index])
-                                remove_car_index = self.car_info['list'].pop([i])
-                                self.car_info['model'].pop([i])
-                                self.spots += 1
-                            if len(self.car_ifo['list']) < my_list:
-                                while True:
-                                    return"You have 15 minutes"
+    def take_ticket(self):
+        ticket = list(empty_spots)[0]
+        garage.update({ticket:empty_spots[ticket]})
+        del empty_spots[ticket]
+        print(f'Your ticket is #{ticket}')
 
-    def cars_in_garage(self):
-        for m in self.car_info.items():
-            print(m)
-                                
+    def pay_ticket(self):
+        while True:
+            open_spot_response = int(input('What is your ticket #?'))
+            if open_spot_response == 0:
+                break
+            elif open_spot_response in garage.keys():
+                response = input(f'Your ticket # is {open_spot_response} is that correct? Y/N')
+                while True:
+                    if response.lower() == 'y':
+                        payment = input('Your total is $10. Debit or Credit')
+                        if payment.lower() == 'debit' or payment.lower() == 'credit':
+                            garage[open_spot_response] = 'Paid'
+                            print('Thank you for your payment')
+                            break
+                        elif response.lower == 'n':
+                            break
+                        else:
+                            print('Try again')
+
+    def leave_parking(self):
+        while True:
+            show_ticket = int(input('Show your ticket #:'))
+            if show_ticket == 0:
+                break
+            elif garage.get(show_ticket) == 'Paid':
+                print('Thank you for coming!! Have a nice day!!')
+                garage[show_ticket] = 'unpaid'
+                empty_spots.update({int(show_ticket):garage[show_ticket]})
+                del garage[show_ticket]
+                break
+            elif garage.get(show_ticket) == 'unpaid':
+                print('Please pay your ticket before leaving')
+
+    def parking_garage(self):
+        while True:
+            answer = input("What would you like to do? 'Spots Open'/'Take Ticket'/'Pay Ticket'/'Leave'/'Quit'")
+            if answer.lower() == 'quit':
+                print('Please visit again!')
+                break
+            elif answer.lower() == 'spots open':
+                self.spots_open()
+            elif answer.lower() == 'take ticket':
+                self.take_ticket()
+            elif answer.lower() == 'pay ticket':
+                self.pay_ticket()
+            elif answer.lower() == 'leave':
+                self.leave_parking()
+            else:
+                print('Sorry, Try again')    
+
                             
                                 
-my_parking_lot = Garage()
-print(my_parking_lot.spots_open())
-my_parking_lot.add_car('2015', 'black', 'pathfinder')
+program = Parking()
+program.parking_garage()
